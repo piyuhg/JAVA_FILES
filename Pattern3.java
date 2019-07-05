94f2d67224dc9a5394dbd9be6775803834d6ddb9
Piyush Gupta <32727401+piyuhg@users.noreply.github.com>
2019-07-05 23:07:36 +0530

    Add files via upload

---
+++ Pattern3.java
@@ -0,0 +1,29 @@
+package patterns;
+
+import java.util.Scanner;
+
+public class Pattern3 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		int i = 1;
+		int val = 1;
+		while(i<=n)		{
+			int j = 1;
+			int k = 1;
+			while(k<=n-i) {
+				System.out.print(" ");
+				k++;
+			}				
+			while(j<=i) {
+				System.out.print(val);
+				j++;
+				val++;
+			}
+			i++;
+			System.out.println();
+		}
+	}
+
+}
