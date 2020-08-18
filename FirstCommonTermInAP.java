b3913029e7b9db61f36f2c149a2ef33536dd4ef0
Piyush Gupta <piyushg.gupta645@gmail.com>
2020-08-18 20:30:25 +0530

    Resolved the errors
    1.  Renamed the findFirstCommonTermInAP() to getFirstCommonTermInAP().
    
    2. Resolved the errors in main()
       --declared the result varible
       --added the call to the function from main.

--- FirstCommonTermInAP.java
+++ FirstCommonTermInAP.java
@@ -23,31 +23,33 @@ public class FirstCommonTermInAP {
 		double i = 0;
 		for(int j = 0; j< 500; j++) {
 			i = (c*j - diff)/(a*1.0);
-			if(i - Math.floor(i) == 0) {
-				break;
-			}
+			if(i - Math.floor(i) == 0)
+				break;	
 		}
-		return (int)(b + a*i);
+			return (int)(b + a*i);
 	}
-	
-	public static void main(String[] args) {
 		
+	public static void main(String[] args) {
+			
 		int a, b, c, d;
 				
 		a=20; b=2; c=9; d=19;
-		System.out.println();
+		
+		int result= getFirstCommonTermInAP(a, b, c, d);
 		if(result == -1) 
 			System.out.println("The AP1 and AP2 do not intersect each other");
 		else
 			System.out.println("The first common term in AP1 and AP2 is: "+result);
 
 		System.out.println("\n-----------------------------\n");
-
+			
 		a=2; b=1; c=16; d=12;
-		System.out.println();
+		result= getFirstCommonTermInAP(a, b, c, d);
+		
 		if(result == -1) 
 			System.out.println("The AP1 and AP2 do not intersect each other");
 		else
 			System.out.println("The first common term in AP1 and AP2 is: "+result);
 	}
+
 }		
