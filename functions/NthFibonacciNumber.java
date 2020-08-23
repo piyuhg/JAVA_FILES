9bc4ff80ba1a7791d420f8c56667c9063798185d
Piyush Gupta <piyushg.gupta645@gmail.com>
2020-08-23 14:38:02 +0530

    Function to find Nth Number in Fibonacci Series

---
+++ functions/NthFibonacciNumber.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+public class NthFibonacciNumber {
+	
+	public static void printNthFibonacciNumber(int n) {
+		int a = 0;
+		int b = 1;
+		int c;
+		for(int i = 0;i<n;i++) {
+			c = a+b;
+			a = b;
+			b = c;
+		}
+		System.out.println(a);
+		return;
+	}
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		
+		int n =  s.nextInt();
+		printNthFibonacciNumber(n);	
+				
+	}
+
+}
